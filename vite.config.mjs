11ee72964525b502eec82dda208260571914e7fe
Jesus Bauza <jesusbauza81@gmail.com>
2025-12-09 22:17:39 -0400

    refactor: rename Vite configuration to `.mjs` and update for ES module compatibility.

---
+++ vite.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+    plugins: [
+        svelte()
+    ],
+    resolve: {
+        alias: {
+            '/$/': path.resolve(__dirname, './src'),
+            '/$collections': path.resolve(__dirname, './src/collections'),
+            '/$components': path.resolve(__dirname, './src/components'),
+            '/$lib': path.resolve(__dirname, './src/lib'),
+        }
+    },
+    optimizeDeps: {
+        include: ['paper/dist/paper-core', 'dayjs/esm', 'dayjs/esm/plugin/calendar'],
+        exclude: ['@roxi/routify']
+    },
+    server: {
+        port: 5000
+    }
+})
