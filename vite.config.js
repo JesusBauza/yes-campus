fd1b9ad3294d36ced81cb4a1dc03fbaf571f39fd
Jesus Bauza <jesusbauza81@gmail.com>
2025-12-09 21:56:06 -0400

    feat: Migrate build system from svite to vite, update Svelte, and ignore .env files.

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,25 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
-// import svite from 'svite'
 
-// const sviteConfig = {
-//   hot: true,
-//   useTransformCache: false, // default is true, but HMR is broken then
-// }
-
-let config = {
-  alias: {
-    '/$/': path.resolve(__dirname, './src'),
-    '/$collections': path.resolve(__dirname, './src/collections'),
-    '/$components/': path.resolve(__dirname, './src/components'),
-    '/$lib/': path.resolve(__dirname, './src/lib'),
+export default defineConfig({
+  plugins: [
+    svelte()
+  ],
+  resolve: {
+    alias: {
+      '/$/': path.resolve(__dirname, './src'),
+      '/$collections': path.resolve(__dirname, './src/collections'),
+      '/$components': path.resolve(__dirname, './src/components'),
+      '/$lib': path.resolve(__dirname, './src/lib'),
+      // Fix for routify if needed, sometimes alias helper helps
+    }
   },
-  // plugins: [
-  //   svite(sviteConfig),
-  // ],
   optimizeDeps: {
-    include: ['paper/dist/paper-core'],
-    exclude: ['@roxi/routify'],
-    include: ['dayjs/esm', 'dayjs/esm/plugin/calendar'],
+    include: ['paper/dist/paper-core', 'dayjs/esm', 'dayjs/esm/plugin/calendar'],
+    exclude: ['@roxi/routify']
   },
-}
-
-export default config
+  server: {
+    port: 5000 // Default svite was probably 5000 or 3000, vite default is 5173 but let's stick to standard if possible or let it pick.
+  }
+})
